10dece87edf9c272fe12d605f3a3980a5316eec5
Karolina Karaskiewicz <77915@student.ue.poznan.pl>
2023-06-22 19:48:47 +0200

    zadanie 9 zrobione

--- solo_work/solo_1.py
+++ solo_work/solo_1.py
@@ -57,3 +57,11 @@ print("Alfabetyczna lista studentow wynosi: ")
 for student in studenci:
     print(student)
 
+#zadanie 1.9
+
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+liczba_n = 0
+for student in studenci:
+    if student.split()[1].startswith("N"):
+        liczba_n += 1
+print("Liczba studentow na N wynosi:", liczba_n)
