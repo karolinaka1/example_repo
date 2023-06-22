966cf3f6f7f09175e77f27a7e468d70b5f636ffa
Karolina Karaskiewicz <77915@student.ue.poznan.pl>
2023-06-22 17:07:38 +0200

    zadanie 1 zrobione

--- solo_work/solo_1.py
+++ solo_work/solo_1.py
@@ -1,11 +1,8 @@
-def trojkat(bok_a, bok_b, bok_c, h_a):
-    pole = bok_a * h_a / 2
-    obwod = bok_a + bok_b + bok_c
-        return pole, obwod
+#zadanie 1
+hello = "Hello"
+student = "Karolina"
+
+komunikat = "{} {}".format(hello, student)
+print(komunikat)
 
 
-print('Pole i obwod wynosza: (trojkat(10, 15, 12, 8))')
-
-def prostokat(a_p, b_p, ):
-    pole =  * b_p
-    obwod = 2 * a_p + 2 * b_p
