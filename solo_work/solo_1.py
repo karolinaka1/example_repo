57b56bc8ed170c43bfca81509487e8273dec8b54
Karolina Karaskiewicz <77915@student.ue.poznan.pl>
2023-06-22 18:54:01 +0200

    zadanie 3 zrobione

--- solo_work/solo_1.py
+++ solo_work/solo_1.py
@@ -1,11 +1,19 @@
-#zadanie 1
+#zadanie 1.1
 hello = "Hello"
 student = "Karolina"
 
 komunikat = "{} {}".format(hello, student)
 print(komunikat)
 
-#zadanie 2
+#zadanie 1.2
 student = input("Podaj imię: ")
 przywitanie = "Hello " + student
 print(przywitanie)
+
+#zadanie 1.3
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+
+liczba_studentow = len(studenci)
+
+print("Liczba studentów wynosi: {}".format(liczba_studentow))
+
